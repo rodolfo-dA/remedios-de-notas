7d3d3d8aa23c7e0910fb10081096c0c7db6df683
Breno <breno_nyx@getnet.com.br>
2025-10-28 21:44:58 -0300

    feat: Criacao da funcao breninho
    A funcao breninho ela faz a contagem dos dasjdqnkqam xzmlcs jkqwdljk qwdjoewqdkoqwdkjwqd q

--- App.js
+++ App.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import HomeScreen from './screens/HomeScreen';
 
+//funcao disso disso daquilo
+function breninho(){
+	const tal = 3;
+	if (1 === 3){
+		//blabla
+	}
+}
+
 export default function App() {
   return <HomeScreen />;
 }
